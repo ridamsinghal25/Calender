d5cf5e5ed906ad7764a4e1f9bbbcc5b737df3caf
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-10-28 21:52:40 +0530

    add controllers to backend

---
+++ backend/src/controllers/event.controller.js
@@ -0,0 +1,105 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { isValidObjectId } from "mongoose";
+import { Event } from "../models/event.model.js";
+
+const createEvent = asyncHandler(async (req, res) => {
+  const { title, start, end, description } = req.body;
+  const userId = req.user._id;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user");
+  }
+
+  if (
+    [title, start, end, description].some(
+      (field) => field?.trim === "" || !field
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const newEvent = await Event.create({
+    title,
+    start,
+    end,
+    description,
+    createdBy: userId,
+  });
+
+  if (!newEvent) {
+    throw new ApiError(500, "Failed to create event");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, newEvent, "Event created successfully"));
+});
+
+const getEvents = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user");
+  }
+
+  const events = await Event.aggregate([
+    {
+      $match: {
+        createdBy: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "createdBy",
+        foreignField: "_id",
+        as: "createdBy",
+        pipeline: [
+          {
+            $project: {
+              password: 0,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$createdBy",
+    },
+    {
+      $sort: {
+        start: 1,
+      },
+    },
+  ]);
+
+  if (events?.length === 0 || !events) {
+    return res.status(200).json(new ApiResponse(200, {}, "No event found"));
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, events, "Events fetched successfully"));
+});
+
+const deleteEvent = asyncHandler(async (req, res) => {
+  const { eventId } = req.params;
+
+  if (!isValidObjectId(eventId)) {
+    throw new ApiError(400, "Invalid event");
+  }
+
+  const deletedEvent = await Event.findByIdAndDelete(eventId);
+
+  if (!deletedEvent) {
+    throw new ApiError(500, "Failed to delete event");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, deletedEvent, "Event deleted successfully"));
+});
+
+export { createEvent, getEvents, deleteEvent };
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,105 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+const generateAccessTokenAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new ApiError(404, "user not found");
+    }
+
+    const accessToken = user.generateAccessToken();
+
+    return { accessToken };
+  } catch (error) {
+    throw new ApiError(500, "Error while generating refresh and access token");
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    [username, email, password].some((field) => field?.trim() === "" || !field)
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    throw new ApiError(400, "User with email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  const createdUser = await User.findById(user._id).select("-password");
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "Email and password is required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, "user not found");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Invalid password");
+  }
+
+  const { accessToken } = await generateAccessTokenAndRefreshToken(user._id);
+
+  const loggedInUser = await User.findById(user._id).select("-password");
+
+  const options = {
+    httpOnly: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+        },
+        "user login successfully"
+      )
+    );
+});
+
+const getCurrentUserDetails = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "user details fetched successfully"));
+});
+
+export { registerUser, loginUser, getCurrentUserDetails };
