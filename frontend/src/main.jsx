4e59099d84cfef76ab352edf87ca68b54195fda5
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-10-29 22:13:26 +0530

    add axios config to main.jsx file in frontend and change .gitignore and App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/toaster";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.withCredentials = true;
+axios.defaults.timeout = 120000;
+
+axios.interceptors.request.use(
+  function (config) {
+    const token = sessionStorage.getItem("token");
+
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
